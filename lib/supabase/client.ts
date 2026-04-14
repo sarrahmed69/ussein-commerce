9e6b8b33318c17a4b4c64b51bab6a69bb29101a2
yamallamin238-gif <tonemailgithub@gmail.com>
2026-04-14 20:31:23 +0000

    fix: hardcode supabase client

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,8 @@
 ﻿import { createBrowserClient } from '@supabase/ssr'
+
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+      "https://koqztlrfxeiolpwyxgum.supabase.co",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvcXp0bHJmeGVpb2xwd3l4Z3VtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxNjMzNjYsImV4cCI6MjA5MDczOTM2Nn0.yjWiPieTaO6sOmTWFSI81DYEds_jKvv2Wnvvr_n4WSI"
   )
 }
