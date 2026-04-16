97fabac3d98939e1a9bd1e75ec001973bf93e491
yamallamin238-gif <tonemailgithub@gmail.com>
2026-04-16 02:25:30 +0000

    fix: routes admin avec try/catch

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -6,3 +6,5 @@ export function createClient() {
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvcXp0bHJmeGVpb2xwd3l4Z3VtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxNjMzNjYsImV4cCI6MjA5MDczOTM2Nn0.yjWiPieTaO6sOmTWFSI81DYEds_jKvv2Wnvvr_n4WSI"
   )
 }
+
+
